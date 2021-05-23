4952c346e031c0942ca81f84db30bc1044230be7
Alwin Blok <alwinb@users.noreply.github.com>
2021-05-23 07:18:33 +0200

    Release 1.2.0

--- src/index.js
+++ src/index.js
@@ -479,7 +479,7 @@ const WHATWGParseResolve = (input, base) => {
 // =======
 
 module.exports = {
-  version: '1.1.1',
+  version: '1.2.0',
   isBase, isResolved,
   ords, ord, upto, goto, 
   preResolve, resolve, force, forceResolve,
