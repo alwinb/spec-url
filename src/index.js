553281200d5f8f6c4c5a888008a4152bf084cb66
Alwin Blok <alwinb@users.noreply.github.com>
2021-11-16 09:42:15 +0100

    modeFor must not return the fallback mode for generic URLs

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ const specials =
   { http:1, https:1, ws:1, wss:1, ftp:1, file:2 }
 
 const modeFor = ({ scheme }, fallback = modes.generic) =>
-  specials [low (scheme)] || fallback
+  scheme ? specials [low (scheme)] || modes.generic : fallback;
 
 const isBase = ({ scheme, host, root }) =>
   scheme != null && (host != null || root != null)
